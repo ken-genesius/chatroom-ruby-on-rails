61ac3a77517a286e760a760be3a9d19f6370b188
ken-genesius <mkengenesius@gmail.com>
2025-03-09 14:11:20 +0700

    fix(devise): Resolve issue with username authentication and email required

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_09_060604) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_09_065808) do
   create_table "users", force: :cascade do |t|
     t.string "user_name", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_09_060604) do
     t.string "user_profile_url", limit: 1000
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
