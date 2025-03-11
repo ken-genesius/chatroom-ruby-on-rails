2c0b713e3eec850de4b853f98eb435acc58a1615
ken-genesius <iyus@LAPTOP-A7KK9GPI.>
2025-03-11 08:18:48 +0700

    feat(chatroom): able to create a new chatroom, join any chatrooms, and send messages in the chatroom

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  scope :all_except, ->(user) { where.not(id: user) }
+  has_many :messages
+  after_create_commit { broadcast_append_to "users" }
+
    def email_required?
     false
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_09_065808) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_10_234940) do
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_private"
+    t.string "image_url", limit: 1000
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.integer "chatroom_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "user_name", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +42,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_09_065808) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
+
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
