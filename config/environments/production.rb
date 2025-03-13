b6bde0f90b049ace9b292ee970d11b30984c68c9
ken-genesius <iyus@LAPTOP-A7KK9GPI.>
2025-03-13 11:49:53 +0700

    fix(storage): changed to digitalocean

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.asset_host = "http://assets.example.com"
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :google
+  config.active_storage.service = :digitalocean
   config.active_storage.variant_processor = :mini_magick
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
